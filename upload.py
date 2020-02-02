1c65b887733c86dea5389f5bc6ab97abeb693c71
Ankit Mathur <ankit.mathur@tobii.com>
2020-02-03 00:05:57 +0100

    Update upload.py

--- upload.py
+++ upload.py
@@ -1,8 +1,9 @@
 from exoscale import Exoscale
+import os
 
 exo = Exoscale("config.toml")
 bucket = exo.storage.get_bucket("bitwarden-bucket")
-file_index = bucket.put_file("/data/db_backup/backup.sqlite3")
-
+file_index = bucket.put_file("/app/database.zip")
 files = bucket.list_files()
 print(next(files))
+os.remove("/app/database.zip")
